3fe78380989a781bb15138b3d6d1e1b43dda244c
Siddharthan <siddharthansp07@gmail.com>
2026-06-03 21:31:43 +0530

    added preview box, made changes to css.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll('.toggle-bar button');
 const snippet_panel = document.getElementById('snippet-panel');
+const preview_img = document.getElementById('preview-img');
 
 buttons.forEach(btn =>
 {
@@ -16,5 +17,17 @@ buttons.forEach(btn =>
         {
             snippet_panel.classList.remove('visible');
         }
+    });
+});
+
+document.querySelectorAll('.snippet-grid img').forEach(img =>
+{
+    img.addEventListener('click', () =>
+    {
+        document.querySelectorAll('.snippet-grid img').forEach(i => i.classList.remove('selected'));
+        img.classList.add('selected');
+
+        preview_img.src = img.src;
+        preview_img.classList.add('visible');
     });
 });
