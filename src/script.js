b050e2fcb13dfb496f3abcdd5fe737b6d43262c5
Siddharthan <siddharthansp07@gmail.com>
2026-06-05 20:11:38 +0530

    Added layers to snipet categories.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,39 @@
 const buttons = document.querySelectorAll('.toggle-bar button');
 // const snippet_panel = document.getElementById('snippet-panel');
 const preview_img = document.getElementById('preview-img');
+const preview_box = document.querySelector('.preview-box');
+const selections = {};
+
+function render_preview()
+{
+    preview_box.querySelectorAll('img').forEach(img => img.remove());
+
+    const layerOrder = 
+    [
+        'Background', 
+        'Character', 
+        'Shirts', 
+        'Eyes', 
+        'Mouth', 
+        'Cheeks', 
+        'Ears', 
+        'Extra'];
+
+    layerOrder.forEach(layer =>
+    {
+        if (selections[layer])
+        {
+            const img = document.createElement('img');
+            img.src = selections[layer];
+            img.style.position = 'absolute';
+            img.style.width = '100%';
+            img.style.height = '100%';
+            img.style.objectFit = 'cover';
+            img.style.display = 'block';
+            preview_box.appendChild(img);
+        }
+    });
+}
 
 function showTab(tabName)
 {
